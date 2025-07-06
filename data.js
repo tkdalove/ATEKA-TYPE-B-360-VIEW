19f4d79ba9d34c687b271e98d84933e89bd91737
tkdalove <sumate4454@gmail.com>
2025-07-06 08:32:12 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,296 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "หน้าบ้าน",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4046393117681166,
+          "pitch": 0.06547036450296417,
+          "rotation": 0,
+          "target": "3--1"
+        },
+        {
+          "yaw": -0.15767181265771768,
+          "pitch": 0.10298526094868166,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "พื้นที่สระว่ายน้ำ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7010653287660666,
+          "pitch": 0.23953303226155676,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.421418804788388,
+          "pitch": 0.15447080457292728,
+          "rotation": 0,
+          "target": "5--2"
+        },
+        {
+          "yaw": -2.0868006215993624,
+          "pitch": 0.27681918231743907,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "ห้องรับแขก",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0051700235979624,
+          "pitch": 0.5708046510527325,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.2500915553611289,
+          "pitch": 0.2846355050549274,
+          "rotation": 0,
+          "target": "3--1"
+        },
+        {
+          "yaw": -2.971621546389569,
+          "pitch": 0.2814495170963127,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": -2.645035258634243,
+          "pitch": 0.3727005840286175,
+          "rotation": 0.7853981633974483,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--1",
+      "name": "ห้องนอน 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.355256226538689,
+          "pitch": 0.27358523110015476,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.068100240906216,
+          "pitch": 0.262182802440849,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--1",
+      "name": "ห้องน้ำ 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--2",
+      "name": "ห้องนอน 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1634039720486289,
+          "pitch": 0.15026167464674067,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.7575360238565807,
+          "pitch": 0.19469872940364752,
+          "rotation": 0.7853981633974483,
+          "target": "6--2"
+        },
+        {
+          "yaw": -2.5942419314263727,
+          "pitch": 0.13852310509401633,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--2",
+      "name": "ห้องน้ำ 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2860876033505768,
+          "pitch": 0.278133002926225,
+          "rotation": 0,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ATEKA TYPE B",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
